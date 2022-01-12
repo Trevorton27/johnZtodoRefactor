d1b1b44b562801df90d15a820b006ba8b4dc08f1
John Ritz <94155021+EncompassingResidential@users.noreply.github.com>
2022-01-12 01:00:20 +0000

    Figured out how to get checkbox checked value and printed out Checkbox HTML properties.  Didn't figure out why innerHTML was blank.

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let tableListRowCounter = 1;
 
   function addToDoItemToTable() {
     let table = document.getElementById("resumeToDoTable");
+  
     let row = table.insertRow(tableListRowCounter);
 
     let dateCell = row.insertCell(0);
@@ -30,7 +31,8 @@ let tableListRowCounter = 1;
 
     dateCell.innerHTML = getDateString();
 
-    checkBoxCell.innerHTML = `<input type="checkbox" id="row${tableListRowCounter}">`;
+    checkBoxCell.innerHTML = `<input type="checkbox" id="row${tableListRowCounter}" title="Added checkbox Title here">Added Checkbox Text</input>`;
+
     tableListRowCounter += 1;
 
     toDoTextCell.innerHTML = document.getElementById("toDoItemText").value;
@@ -38,34 +40,44 @@ let tableListRowCounter = 1;
   }
 
   function deleteToDoItemToTable() {
-    let table, rows, switching, i, x, y, shouldSwitch;
-    let consoleText = "";
-
-    table = document.getElementById("resumeToDoTable");
-    rows = table.rows;
-
-    consoleText = "about to loop through table list : # of rows = " + rows.length;
 
     let cb = document.querySelector('#row0');
-    console.log("row0 checkBox.checked is " + cb.checked + ".");
-
-    // console.log(consoleText);
-
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
-    for (i = 0; i < rows.length; i++) {
-
-      checkBox = rows[i].getElementsByTagName("TD")[1].innerHTML;
-
-      consoleText += "<br>\n" + i + " checkBox is " + checkBox.id + " and has " + checkBox.length;
-
-      // document.getElementById("demo").innerHTML = consoleText;
-
-      toDoItemTextString = rows[i].getElementsByTagName("TD")[2].innerHTML;
-
+    console.log(" row0 checkBox.checked is " + cb.checked + ".");
+    if (document.querySelector('#row1')) {
+      console.log(" row1 is " + document.querySelector('#row1').checked);
     }
 
-    console.log("A " + consoleText);
+    let inputs_in_table = document.getElementById("resumeToDoTable").getElementsByTagName("input");
+
+    inputs_in_table = document.getElementById("resumeToDoTable").getElementsByTagName("input");
+
+    console.log("   About to loop through table list : # of rows = " + inputs_in_table.length);
+
+    for (var jLoop = 0; jLoop < inputs_in_table.length; jLoop++) {
+      let checkBox2 = inputs_in_table[jLoop];
+
+      console.log(`\njLoop inputs_in_table[${jLoop}].type <` + inputs_in_table[jLoop].type + ">");
+
+      if (inputs_in_table[jLoop].type == "checkbox") { 
+          console.log(`    if jLoop ${jLoop} checkBox2.checked = <${checkBox2.checked}>`);
+          console.log(" and checkBox2.checked is " + checkBox2.checked);
+          console.log(" and checkBox2.innerHTML is '" + checkBox2.innerHTML + "' should be there");
+          console.log(" checkBox2.length       is " + checkBox2.length);
+          console.log(" and checkBox2.parentNode is " + checkBox2.parentNode);
+          console.log(" and checkBox2.parentNode.parentNode is " + checkBox2.parentNode.parentNode);
+          console.log(" and checkBox2.parentNode.parentNode.parentNode is " + checkBox2.parentNode.parentNode.parentNode);
+          console.log(" and checkBox2.parentNode.parentNode.parentNode.parentNode is " + checkBox2.parentNode.parentNode.parentNode.parentNode);
+          console.log(" and checkBox2.tagName  is " + checkBox2.tagName);
+          console.log(" and checkBox2.text     is '" + checkBox2.text + "' should be there");
+          console.log(" and checkBox2.textContent is '" + checkBox2.textContent + "' should be there");
+          console.log(" and checkBox2.title    is '" + checkBox2.title + "' should be there");
+          console.log(" and checkBox2.toString is " + checkBox2.toString());
+          console.log(" and checkBox2.type     is " + checkBox2.type);
+          console.log(" and checkBox2.value    is '" + checkBox2.value + "'");
+      }
+    }
+
+    console.log("\nend of Delete ");
 
   }
 
