820d848556a83e20b07e54c923c75234c618ba8f
John Ritz <94155021+EncompassingResidential@users.noreply.github.com>
2022-01-13 20:55:19 +0000

    Okay, don't except for not calling change event function even thought it got called on creation?!

--- script.js
+++ script.js
@@ -3,6 +3,55 @@ const daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","S
 let tableListRowCounter = 1;
 let eventMaxListenerRowCounter = 1;
 
+  function printElement(elementHTML) {
+    console.log("START printElement");
+
+    if (typeof(elementHTML) != 'undefined' && elementHTML != null) {
+
+      if (typeof(elementHTML.checked) != 'undefined' && elementHTML.checked != null) {
+        console.log(`    if elementHTML.checked = <${elementHTML.checked}>`);  
+        console.log(" and elementHTML.checked    is " + elementHTML.checked);
+      }
+      console.log(" and elementHTML.id         is '" + elementHTML.id + "' should be there");
+      console.log(" and elementHTML.innerHTML  is '" + elementHTML.innerHTML + "' should be there");
+      console.log(" and elementHTML.length     is " + elementHTML.length);
+
+      if (typeof(elementHTML.parentNode) != 'undefined' && 
+                  elementHTML.parentNode != null) {
+        console.log(" and elementHTML.parentNode is " + elementHTML.parentNode);
+      
+        if (typeof(elementHTML.parentNode.parentNode) != 'undefined' &&
+                    elementHTML.parentNode.parentNode != null) {
+          console.log(" and elementHTML.parentNode.parentNode exists and is " + elementHTML.parentNode.parentNode);
+
+          if (typeof(elementHTML.parentNode.parentNode.parentNode) != 'undefined' && 
+                      elementHTML.parentNode.parentNode.parentNode != null) {
+            console.log(" and elementHTML.parentNode.parentNode.parentNode exists and is " + 
+            elementHTML.parentNode.parentNode.parentNode);
+
+            if (typeof(elementHTML.parentNode.parentNode.parentNode.parentNode) != 'undefined' && 
+                        elementHTML.parentNode.parentNode.parentNode.parentNode != null) {
+              console.log(" and elementHTML.parentNode.parentNode.parentNode.parentNode  exists and is " + elementHTML.parentNode.parentNode.parentNode.parentNode);
+            }
+          }
+        }
+      }
+
+      console.log(" and elementHTML.tagName    is " + elementHTML.tagName);
+      console.log(" and elementHTML.text       is '" + elementHTML.text + "' should be there");
+      console.log(" and elementHTML.textContent is '" + elementHTML.textContent + "' should be there");
+      console.log(" and elementHTML.title      is '" + elementHTML.title + "' should be there");
+      console.log(" and elementHTML.toString   is " + elementHTML.toString());
+      console.log(" and elementHTML.type       is " + elementHTML.type);
+      console.log(" and elementHTML.value      is '" + elementHTML.value + "'");
+    }
+    else {
+      console.log(`printElement HTML element was undefined or did not exists === NULL`);
+    }
+
+    console.log("END printElement");
+  }
+
   function getDateString() {
     const today = new Date();
     const dayOfWeek = today.getDay();
@@ -23,13 +72,17 @@ let eventMaxListenerRowCounter = 1;
 
   function checkBoxClicked(rowString) {
 
-    console.log(`In function checkBoxClicked(rowString is ${rowString}) ` + getTimeString());
+    console.log(`\nIn function checkBoxClicked(rowString is ${rowString}) ` + getTimeString());
 
     let checkBox = document.getElementById(rowString);
+
+    // printElement(checkBox);
 
     console.log("checkBoxClicked looking for `${rowString}text` is " + `${rowString}text`);
 
     let toDoText = document.getElementById(`${rowString}text`);
+
+    // printElement(toDoText);
 
     if (typeof(toDoText) != 'undefined' && toDoText != null) {
       console.log(`checkBoxClicked toDoText exists and toDoText.id = ${toDoText.id}`);
@@ -67,14 +120,17 @@ let eventMaxListenerRowCounter = 1;
 
     checkBoxCell.innerHTML = `<input type="checkbox" id="row${eventMaxListenerRowCounter}" title="Added checkbox Title here" value="Added checkbox value"></input>`;
 
-    console.log("calling addEventListener checkBoxClicked(`row${eventMaxListenerRowCounter}`) is " + `row${eventMaxListenerRowCounter}`);
+    console.log("\ncalling addEventListener checkBoxClicked(`row${eventMaxListenerRowCounter}`) is " + `row${eventMaxListenerRowCounter}`);
 
-    document.getElementById(`row${eventMaxListenerRowCounter}`).addEventListener("click", checkBoxClicked(`row${eventMaxListenerRowCounter}`) );
-
+    // Had to put this BEFORE the addEventListener call
     toDoTextCell.innerHTML = document.getElementById("toDoItemText").value;
-    toDoTextCell.id = `row${eventMaxListenerRowCounter}`;
+    toDoTextCell.id = `row${eventMaxListenerRowCounter}text`;
 
-    conosole.log(toDoTextCell);
+    document.getElementById(`row${eventMaxListenerRowCounter}`).addEventListener(
+      "click", checkBoxClicked(`row${eventMaxListenerRowCounter}`) 
+      );
+
+    console.log("\n" + getTimeString() + "  Table Row text field.id is '" + toDoTextCell.id + "' has '" + toDoTextCell.innerHTML + "'");
 
     tableListRowCounter++;
 
